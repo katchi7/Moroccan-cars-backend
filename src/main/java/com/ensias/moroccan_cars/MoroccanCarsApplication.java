777292ea2227027461d5423497c3bc69599dc6ed
Abderrahim <abderrahimdeaabane@gmail.com>
2021-06-13 18:41:58 +0100

    First commit

---
+++ src/main/java/com/ensias/moroccan_cars/MoroccanCarsApplication.java
@@ -0,0 +1,13 @@
+package com.ensias.moroccan_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoroccanCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoroccanCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensias/moroccan_cars/MoroccanCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensias.moroccan_cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroccanCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
